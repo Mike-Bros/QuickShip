d9f690c8430fedf77cc9be933189b1f5017046c3
Mike Bros <mbros@roofmaxx.com>
2022-06-07 14:14:05 -0500

    created PackageService.py to ingest package list from CSV

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from models.Package import Package
 from models.Truck import Truck
+from services import PackageService
 
 if __name__ == '__main__':
-    p1 = Package(1, "1948 Circle", "Savage", "MN", 55811, "EOD", 3, "This is a special note")
-    p2 = Package(2, "300 State St", "Prior Lake", "MN", 55821, "EOD", 2,)
-    p3 = Package(3, "600 E 900 South", "Holladay", "MN", 53765, "10:30 AM", 2,)
-    #p1.print()
+    package_list = PackageService.ingest_packages()
 
     t1 = Truck()
-    t1.load_package(p1)
-    t1.load_package(p2)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.load_package(p3)
-    t1.print()
 
+    for package in package_list:
+        if t1.can_add_package():
+            t1.load_package(package)
+
+    t1.print()
--- models/Truck.py
+++ models/Truck.py
@@ -6,7 +6,7 @@ class Truck:
         self.seperator = "--------------------------------------------------------------------------------"
 
     def load_package(self, package):
-        if self.current_capacity() <= 15:
+        if self.can_add_package():
             self.packages.append(package)
             print("Added package: " + str(package.id) + " to truck. Current Capacity: " + str(self.current_capacity()))
         else:
@@ -17,7 +17,7 @@ class Truck:
         heading = "Truck Details"
         stats = "Max Capacity: " + str(self.max_capacity) \
                 + " | Current Capacity: " + str(self.current_capacity()) \
-                + " | Avg Speed: " + str(self.avg_speed) \
+                + " | Avg Speed: " + str(self.avg_speed)
 
         print(self.seperator)
         print(heading)
@@ -29,3 +29,9 @@ class Truck:
 
     def current_capacity(self):
         return len(self.packages)
+
+    def can_add_package(self):
+        if self.current_capacity() <= 15:
+            return True
+        else:
+            return False
