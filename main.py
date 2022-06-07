9db78d121f821df1878a906ddb0eba469eb03abd
Mike Bros <mbros@roofmaxx.com>
2022-06-07 14:21:16 -0500

    changed PackageService into a class not just a file of helper functions, on creation of the PackageService class instance the package_list is created and accessible

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from models.Package import Package
 from models.Truck import Truck
-from services import PackageService
+from services.PackageService import PackageService
 
 if __name__ == '__main__':
-    package_list = PackageService.ingest_packages()
+
+    package_list = PackageService().package_list
 
     t1 = Truck()
-
     for package in package_list:
         if t1.can_add_package():
             t1.load_package(package)
-
     t1.print()
--- services/PackageService.py
+++ services/PackageService.py
@@ -2,11 +2,16 @@ from models.Package import Package
 import pandas as pd
 
 
-def ingest_packages():
-    df = pd.read_csv('./packages.csv', keep_default_na="")
-    package_list = []
-    for index, row in df.iterrows():
-        new_package = Package(row['ID'], row['Address'], row['City'], row['State'], row['Zip'], row['Deadline'],
-                              row['Mass'], row['Notes'])
-        package_list.append(new_package)
-    return package_list
+class PackageService:
+    def __init__(self):
+        self.package_list = []
+        self.ingest_packages()
+
+    def ingest_packages(self):
+        print("Ingesting packages...")
+        df = pd.read_csv('./packages.csv', keep_default_na="")
+        for index, row in df.iterrows():
+            new_package = Package(row['ID'], row['Address'], row['City'], row['State'], row['Zip'], row['Deadline'],
+                                  row['Mass'], row['Notes'])
+            self.package_list.append(new_package)
+        print("Finished ingesting packages")
